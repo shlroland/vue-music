4461b528b643a911516dac4a85606d3e1032d991
shlroland <shlroland1995@gmail.com>
2019-10-04 21:52:19 +0800

    修改后台代码

--- prod.server.js
+++ prod.server.js
@@ -10,7 +10,7 @@ var app = express()
 
 var apiRoutes = express.Router()
 
-apiRoutes.get('/getDiscList', function (req, res) {
+apiRoutes.get('/api/getDiscList', function (req, res) {
   var url = 'https://c.y.qq.com/splcloud/fcgi-bin/fcg_get_diss_by_tag.fcg'
   axios.get(url, {
     headers: {
